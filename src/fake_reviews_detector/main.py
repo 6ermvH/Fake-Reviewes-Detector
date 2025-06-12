2ed9815d4926ed8286505819fa57346183ef1d65
German Feskov <g.feskov@yandex.ru>
2025-06-12 14:24:21 +0300

    fix: main

--- src/fake_reviews_detector/main.py
+++ src/fake_reviews_detector/main.py
@@ -1,11 +1,12 @@
+from nltk import data
+from numpy.lib import utils
+from pandas._config.display import cf
 from fake_reviews_detector.data_loader import download_kaggle_dataset
+from fake_reviews_detector.preprocessing import *
 from fake_reviews_detector.utils import load_yaml_config
-import os
 
-cfg = load_yaml_config("config/local_dev.yaml")["dataset"]
-# cfg["raw_data_dir"] — папка, cfg["raw_data_file"] — имя файла
-target_path = os.path.join(cfg["raw_data_dir"], cfg["raw_data_file"])
+cfg = load_yaml_config('config/local_dev.yaml')
 
-# при таком вызове файл внутри папки будет переименован в cfg["raw_data_file"]
-download_kaggle_dataset(cfg["image"], target_path)
+dataset = download_kaggle_dataset('mexwell/fake-reviews-dataset')
 
+create_processed_csv(dataset, cfg, cfg['dataset_path'])
