89cf050d3770c0cc4eafd312e66a1dbf037a8b2e
Tapochek2894 <dvrogov06@yandex.ru>
2025-06-12 14:58:12 +0300

    add: predict button

--- src/fake_reviews_detector/front.py
+++ src/fake_reviews_detector/front.py
@@ -2,7 +2,7 @@
 
 
 from utils import load_yaml_config
-from preprocessing import preprocess_dataset
+from preprocessing import create_processed_csv
 # import data_loader
 # import model
 import tkinter as tk
@@ -47,7 +47,7 @@ def create_gui(root, status_bar=None) -> None:
 
     # Кнопка и поле для файла обучения
     ttk.Label(data_frame, text=config['ui_text']['labels']['train_file']).grid(row=0, column=0, sticky=tk.W)
-    train_file_entry = ttk.Entry(data_frame, width=50)
+    train_file_entry = ttk.Entry(data_frame, width=87)
     train_file_entry.grid(row=0, column=1, padx=5)
     ttk.Button(data_frame, text=config['ui_text']['buttons']['browse'],
                command=lambda: browse_train_file(train_file_entry, status_bar)).grid(row=0, column=2)
@@ -62,7 +62,7 @@ def create_gui(root, status_bar=None) -> None:
     ttk.Button(train_frame, text=config['ui_text']['buttons']['train'],
                command=lambda: train_model(progress, status_bar)).pack(pady=5)
 
-    # Окно предсказания
+    # Фрейм предсказания
     predict_frame = ttk.LabelFrame(main_frame, text=config['ui_text']['main']['prediction'], padding=10)
     predict_frame.pack(fill=tk.BOTH, expand=True, pady=5)
 
@@ -71,25 +71,25 @@ def create_gui(root, status_bar=None) -> None:
     predict_text = tk.Text(predict_frame, height=5, width=80)
     predict_text.pack(fill=tk.X, pady=5)
 
-    # Загрузка файла для предсказания
+    # Или загрузка файла для предсказания
     ttk.Label(predict_frame, text=config['ui_text']['labels']['input_file']).pack(anchor=tk.W)
 
     file_predict_frame = ttk.Frame(predict_frame)
     file_predict_frame.pack(fill=tk.X, pady=5)
 
-    predict_file_entry = ttk.Entry(file_predict_frame, width=50)
+    predict_file_entry = ttk.Entry(file_predict_frame, width=110)
     predict_file_entry.pack(side=tk.LEFT, padx=5)
     ttk.Button(file_predict_frame, text=config['ui_text']['buttons']['browse'],
                command=lambda: browse_predict_file(predict_file_entry, status_bar)).pack(side=tk.LEFT)
 
     # Кнопка предсказания
-    results_text = tk.Text(predict_frame, height=10, width=80, state=tk.DISABLED)
-    results_text.pack(fill=tk.BOTH, expand=True)
-
-    ttk.Label(predict_frame, text=config['ui_text']['labels']['results']).pack(anchor=tk.W)
-
     ttk.Button(predict_frame, text=config['ui_text']['buttons']['predict'],
                command=lambda: make_prediction(predict_text, predict_file_entry, results_text, status_bar)).pack(pady=5)
+
+    # Метка и область результатов
+    ttk.Label(predict_frame, text=config['ui_text']['labels']['results']).pack(anchor=tk.W)
+    results_text = tk.Text(predict_frame, height=10, width=80, state=tk.DISABLED)
+    results_text.pack(fill=tk.BOTH, expand=True)
 
     # Статус бар
     status_bar = ttk.Label(main_frame, text=config['ui_text']['status']['ready'], relief=tk.SUNKEN)
@@ -132,22 +132,17 @@ def browse_predict_file(entry_widget, status_bar) -> None:
 # Сделать предсказание
 def make_prediction(text_widget, file_entry, results_text, status_bar) -> None:
     global model, model_trained
-
     if not model_trained:
         messagebox.showerror("Error", "Please, train model")
         return
-
     input_text = text_widget.get("1.0", tk.END).strip()
     file_path = file_entry.get().strip()
-
     if not input_text and not file_path:
         messagebox.showerror("Error", "Please enter text or select a file for prediction.")
         return
-
     try:
         results_text.config(state=tk.NORMAL)
         results_text.delete("1.0", tk.END)
-
         if input_text:
             update_status("Processing input string...", status_bar)
             try:
@@ -158,24 +153,19 @@ def make_prediction(text_widget, file_entry, results_text, status_bar) -> None:
             except Exception as e:
                 results_text.insert(tk.END, f"Error: Invalid string format.\n")
                 update_status("Error in format of entered string", status_bar)
-
         if file_path:
             update_status(f"Processing file {os.path.basename(file_path)}...", status_bar)
             try:
                 test_data = pd.read_csv(file_path)
                 predictions = model.predict(test_data)
-
                 results_text.insert(tk.END, "Prediction result:\n")
                 for i, pred in enumerate(predictions, 1):
                     results_text.insert(tk.END, f"String {i}: {pred}\n")
-
                 update_status(f"Prediction fulfilled for file {os.path.basename(file_path)}", status_bar)
             except Exception as e:
                 results_text.insert(tk.END, f"Error processing file: {str(e)}\n")
                 update_status("Error processing file", status_bar)
-
         results_text.config(state=tk.DISABLED)
-
     except Exception as e:
         update_status("Error in prediction execution", status_bar)
         messagebox.showerror("Error", f"Failed to fulfill prediction: {str(e)}")
@@ -197,13 +187,13 @@ def train_model(progress_bar, status_bar) -> None:
         raw_data = pd.read_csv(train_file_path)
         progress_bar['value'] = 10
         update_status("Предобработка данных...", status_bar)
-        processed_data = preprocess_dataset(raw_data)
+        processed_data = create_processed_csv(raw_data)
         if processed_data is None or processed_data.empty:
             raise ValueError("Предобработка вернула пустые данные")
         progress_bar['value'] = 50
         update_status("Обучение модели...", status_bar)
 
-        # Код модели
+        # Обучение модели
 
         progress_bar['value'] = 80
         update_status("Финализация модели...", status_bar)
